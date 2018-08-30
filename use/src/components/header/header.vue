685557eef630d47836058f76bfd0487d7580f214
yep <1486243761@qq.com>
2018-08-30 22:31:57 +0800

    newvue

---
+++ use/src/components/header/header.vue
@@ -0,0 +1,37 @@
+<template>
+	<h1 v-if="!this.$route.query.tittle">{{msg}}</h1><!-- 默认值 -->
+	<h1 v-else>{{this.$route.query.tittle}}</h1>
+</template>
+
+<script>
+	export default{
+	    data () {
+	      return {
+	        msg: '首页'
+	      }
+	    }
+	}
+</script>
+
+<style scoped="">
+	body{
+		width:100%;
+		margin: 0px;
+		padding: 0px;
+	}
+	h1{
+		width: 100%;
+		margin: 0px;
+		padding:5px 0px 10px 0px;
+		color: #fff;
+		text-align: center;
+		box-shadow:0px 3px  3px 1px #eef;
+		background: orange;
+		position: fixed;
+		top: 0px;
+		left: 0px;
+		z-index: 10;
+		font-size:18px;	
+	}
+	
+</style>
---
+++ use/src/components/news/more.vue
@@ -0,0 +1,18 @@
+<template>
+	<h1>{{msg}}</h1>
+</template>
+
+<script>
+	export default{
+	    data () {
+	      return {
+	        msg: '这里还没做'
+	      }
+	    }
+
+	}
+</script>
+
+<style scoped="">
+
+</style>
